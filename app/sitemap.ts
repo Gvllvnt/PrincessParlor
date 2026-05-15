e445e31fef71a1c4014ce27f003a3e1f741620d3
Gvllvnt <165991754+Gvllvnt@users.noreply.github.com>
2026-05-15 15:07:27 +0200

    sitemap fix

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://dezmakgroup.vercel.app/",
+      url: "https://dezmakgroup.vercel.app",
       lastModified: new Date(),
     },
   ];

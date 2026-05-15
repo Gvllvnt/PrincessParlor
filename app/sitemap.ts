30f12575f8ccbba578dd488bb4dcb301551b5d5f
Gvllvnt <165991754+Gvllvnt@users.noreply.github.com>
2026-05-15 15:10:12 +0200

    sitemap fix

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://dezmakgroup.vercel.app",
+      url: "https://princess-parlor.vercel.app",
       lastModified: new Date(),
     },
   ];
